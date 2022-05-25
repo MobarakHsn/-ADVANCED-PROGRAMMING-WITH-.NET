4822b2571b8b3dcac829d9ac12e5a8e731600e00
Mobarak Hossain <mobarok.hossain.9239@gmail.com>
2022-05-25 07:47:51 +0600

    HomeTask1

---
+++ Home Tasks/HomeTask1/HomeTask1/Controllers/CVController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeTask1.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Personal_Info()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
